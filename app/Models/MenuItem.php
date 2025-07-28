08d32a76f0556cb4451265061632d2933b137ca7
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-07-28 21:51:46 +0200

    feat: define $fillable property of menu item, menu category, post, and image models

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\MenuItemFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'price_sm', 'price_md', 'price_lg', 'image_id', 'menu_category_id'];
+}
