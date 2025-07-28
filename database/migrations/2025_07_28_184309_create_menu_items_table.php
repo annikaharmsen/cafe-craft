72268ba431de41869b34e6d172038437f4103cb0
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-07-28 21:37:02 +0200

    feat: implement menu item, menu category, and post migrations

---
+++ database/migrations/2025_07_28_184309_create_menu_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\MenuCategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->int('priceSm')->nullable();
+            $table->int('priceMd')->nullable();
+            $table->int('priceLg')->nullable();
+            $table->string('img_url')->nullable();
+            $table->foreignIdFor(MenuCategory::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};
